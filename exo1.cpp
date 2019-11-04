78481372e16e50bea7ae208858f8a7cda0c56069
eddy <edwardoblette@gmail.com>
2019-11-05 00:23:38 +0100

    facon propre mais pas opti

--- exo1.cpp
+++ exo1.cpp
@@ -2,87 +2,85 @@
 #include <iostream>
 #include <unistd.h>
 #include <thread>
-#include <mutex>
 #include <vector>
-#include <string>
-#include <bitset>
 #include <math.h>  
-#include <chrono>
-#include <string>
 #include <fstream>
+#include <mutex>
 
-
-#define SetTo1(n) (*(list+(n>>3)) |=(1<<(n&7)))
-#define GetBit(n) (*(list+(n>>3)) & (1<<(n&7)))  
-#define LIMIT 1000000000
-#define THREAD 6
-static unsigned char list[LIMIT/8];
-
-//Protos
-void prime(int, int, int id );
-void result(int );
 
 std::mutex block_mutex; 
+std::vector<bool> list(1,1); 
+
+//Protos
+void crible(unsigned long limit, int THREAD);
+void prime(int, int, unsigned long LIMIT, int THREAD);
+void result(unsigned long LIMIT);
 
 
-int main() {  
 
-  int sqrt_limit = sqrt(LIMIT) ;
-  unsigned long   count;
-  std::vector<std::thread> process; 
-
-  int start = 3 , end;
-  for(int i = 0; i < THREAD; i++){
-    end = sqrt_limit * ((double)(i+1)/(THREAD*(THREAD-i)));
-    //prime(start, end );
-    std::cout << start <<" "<< end << std::endl;
-    process.push_back(std::thread(prime, start, end, i ));
-    start = end + 1;
+int main(int argc, char ** argv) {  
+  if(argc != 3){
+    std::cout << "usage : THREAD LIMIT" << std::endl;
+    exit(0);
   }
 
-  //join
-  for(int nbThread = 0; nbThread < THREAD; process[nbThread++].join());
- 
-  result(sqrt_limit);
-  // Result 
-  count=1;
-  for (unsigned long i = 3; i < LIMIT; i += 2) {
-    if(!GetBit(i)){
-     // std::cout << i << std::endl;
-      ++count;
-    }
-  }
-  printf("Count    : %lu\n", count);
- 
+  int THREAD = atoi(argv[1]);
+  unsigned long LIMIT = atoi(argv[2]);
+  list.reserve(LIMIT);
+  std::fill(list.begin(), list.end()+ LIMIT, 1);
+
+  crible(LIMIT, THREAD);
+  result(LIMIT);
+
   return 0;
 }
 
+void crible(unsigned long LIMIT, int THREAD){
 
-void prime(int  start, int end, int id){
-  clock_t begin = clock();
-  unsigned long  i, j, count=0;
-  for ( i = start; i < end; i+=2){
-    if(!GetBit(i))
-      for(j = i*i; j < LIMIT; j += i<<1){
-        ++count;
-        SetTo1(j);
-      }
-      
+  int start , end = sqrt(LIMIT);
+  std::vector<std::thread> process; 
+
+  for(int i = 0; i < THREAD; i++){
+      //end = sqrt_limit * ((double)(i+1)/THREAD);
+      //end = sqrt_limit * ((double)(i+1)/(THREAD*(THREAD-i)));
+      start = 3 + (2 * i);
+      process.push_back(std::thread(prime, start, end, LIMIT, THREAD ));
   }
-  clock_t stop = clock();
-  double elapsed_secs = double(stop - begin) / CLOCKS_PER_SEC;
-  std::cout << id << "stop  working - " << count << " - " << elapsed_secs << std::endl;
+  for(int nbThread = 0; nbThread < THREAD; process[nbThread++].join());
+
+
 }
 
-void result(int sqrt_limit){
-  prime(3, sqrt_limit, 0);
-  
+void prime(int  start, int end, unsigned long LIMIT, int THREAD){
+  unsigned long  i, j;
+  int offset = (2 * THREAD);
+  for ( i = start; i < end; i += offset){
+    block_mutex.lock();
+    if(list[i])
+      for(j = i*i; j < LIMIT; j += i)
+        list[j] = 0;  
+      
+    block_mutex.unlock();  
+  }
+
+ 
+}
+
+void result(unsigned long LIMIT){
+
+  std::cout << "Writing on buffer" << std::endl;
   std::string buffer;
   buffer.reserve(LIMIT);
-  for (unsigned long i = 3; i < LIMIT; i += 2) 
-      if(!GetBit(i))
-        buffer.append(std::to_string(i) + '\n');
+  unsigned long i, count = 1;
+  for ( i = 3; i < LIMIT; i += 2)
+    if(list[i]){
+      //printf("i= %d\n", i);
+      buffer.append(std::to_string(i) + '\n');
+      ++count;
+    }
+  
 
+  std::cout << "Writing " << count << " numbers on file" << std::endl;
   
   std::ofstream myfile ("result.txt");
   if (myfile.is_open()){
